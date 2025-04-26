02133263cc73dd31d6319d293914ef87490afbf0
yash <ymandlekar9826@gmail.com>
2025-04-26 12:36:47 +0530

    Refactor History model schema to use built-in timestamps option and improve code readability

--- models/History.js
+++ models/History.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
-const historySchema = new mongoose.Schema({
-  url: { type: String, required: true },
-  result: { type: String, required: true },
-  userId: { type: String },
-  deviceId: { type: String },
-  timestamp: { type: Date, default: Date.now },
-});
+const historySchema = new mongoose.Schema(
+  {
+    url: { type: String, required: true },
+    result: { type: String, required: true },
+    userId: { type: String },
+    deviceId: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("History", historySchema);
