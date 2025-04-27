55eef568b7edcdf9f335d01be622df6629f8a08c
yash <ymandlekar9826@gmail.com>
2025-04-27 22:52:43 +0530

    Update CORS middleware configuration to specify allowed origin and enable credentials

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // your frontend URL
+  credentials: true, // allow session cookies to be sent
+}));
+
 app.use(express.json());
 app.use(session({
   secret: process.env.SESSION_SECRET,
