e40b6c846cb921bd9d5aca888097c7b0cb091b95
yash <ymandlekar9826@gmail.com>
2025-04-28 11:22:01 +0530

    Add user name and profile image fields to User model; update Google OAuth strategy to store additional user information

--- auth/passport.js
+++ auth/passport.js
@@ -16,6 +16,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "/google/callback",
+      scope: ['profile', 'email'],
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
@@ -26,6 +27,8 @@ passport.use(
         const newUser = await User.create({
           googleId: profile.id,
           email: profile.emails[0].value,
+          name: profile.displayName,
+          profileImage: profile.photos[0].value,
         });
 
         done(null, newUser);
--- models/User.js
+++ models/User.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   googleId: { type: String, required: true },
-  email: { type: String, required: true }
+  email: { type: String, required: true },
+  name: { type: String },
+  profileImage: { type: String },
 });
 
 const User = mongoose.model("User", userSchema);
